eb4f51f30365340225a5086439928b2c57c1742a
laura.dondiego <lauradondiego@gmail.com>
2019-07-26 17:31:35 -0400

    final styling tweaks to my character card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,26 +1,24 @@
 import React from "react";
 import { Card, Icon, Image } from "semantic-ui-react";
 
-const src = "../Img/cardImg.jpg";
-
 export const CardExampleCard = ({ char }) => (
   <Card>
-    <Image
-      // src="https://rickandmortyapi.com/api/character/{avatar/1}.jpeg"
-      src={char.image}
-      style={{ width: "100px" }}
-    />{" "}
+    <Image src={char.image} style={{ width: "100px" }} />{" "}
     <div className="name">
       <h2>Name: {char.name}</h2>
     </div>
-    <div className="Status">
-      <p>Status: {char.status}</p>
+    <div className="Human">
+      <p>Human {char.status}</p>
     </div>
     <div className="location">
       <p>Location: {char.location.name}</p>
     </div>
     <div className="origin">
       <p>Origin: {char.origin.name}</p>
+    </div>
+    <div className="episodes">
+      <Icon name="user" /> Episodes
+      {/* <Link to={EpisodeCard}> </Link> */}
     </div>
   </Card>
 );
