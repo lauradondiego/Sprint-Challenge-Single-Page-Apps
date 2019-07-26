8509f0cbc5f0b2ab5f37f38631ed54efddd304ee
laura.dondiego <lauradondiego@gmail.com>
2019-07-26 11:48:46 -0400

    readme questions done

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
-}
+const src = "../Img/cardImg.jpg";
+
+export const CardExampleCard = ({ char }) => (
+  <Card>
+    {/* <Image src={cardImage} style={{ width: "100px" }} />{" "} */}
+    <div className="name">
+      <h2>Name: {char.name}</h2>
+    </div>
+    <div className="birth">
+      <p>Birth Year: {char.birth_year}</p>
+    </div>
+    <div className="eyecolor">
+      <p>Eye Color: {char.eye_color}</p>
+    </div>
+    <div className="mass">
+      <p>Mass: {char.mass}</p>
+    </div>
+  </Card>
+);
+
+// export default function CharacterCard({ props }) {
+//   return <span>todo: location</span>;
+// }
